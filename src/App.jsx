b3267a0a22111d77736d8f1d2f0c6be15889fc8f
Prateek <prateek.kumar.ind@gmail.com>
2025-10-05 13:53:50 +0530

    add router config to app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import RootLayout from "./pages/RootLayout"
+import HomePage from "./pages/HomePage"
+import ProductDetails from "./pages/ProductDetails"
+import Cart from "./pages/Cart"
+
 
 const App = () => {
+  const router = createBrowserRouter([
+    {path: '/', element: <RootLayout/>, children: [
+      {index: true, element: <HomePage/>},
+      {path: 'product/:id', element: <ProductDetails/>},
+      {path: 'cart', element: <Cart/>}
+    ]},
+    
+  ])
+
   return (
-    <div>App</div>
+    <RouterProvider router={router}/>
   )
 }
 
