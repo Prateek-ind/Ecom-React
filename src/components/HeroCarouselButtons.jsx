2f67ea2f8d0e6cf8dfa91e8dfbea5b852f1a1a67
Prateek <prateek.kumar.ind@gmail.com>
2025-10-07 14:36:32 +0530

    create carousel buttons

---
+++ src/components/HeroCarouselButtons.jsx
@@ -0,0 +1,12 @@
+const HeroCarouselButtons = ({index, currentIndex, setCurrentIndex, className}) => {
+  return (
+    <button className={className} onClick={() => setCurrentIndex(index)}>
+      <div className=" w-1 rounded-full border-2 border-gray-500 "></div>
+      {index === currentIndex && (
+        <div className="absolute inset-0 rounded-full  border-2 border-gray-700 border-t-white animate-spin-slow"></div>
+      )}
+    </button>
+  );
+};
+
+export default HeroCarouselButtons;
