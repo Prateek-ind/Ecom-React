27666a304eba1f0355ff904644bc102596e261cf
Prateek <prateek.kumar.ind@gmail.com>
2025-10-08 12:25:01 +0530

    add poppins font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-  extend: {
-    animation: {
-      'spin-slow': 'spin 6s linear infinite',
+    extend: {
+      fontFamily: {
+        customFont: ['"Poppins"', "sans-serif"],
+      },
+      animation: {
+        "spin-slow": "spin 4s linear infinite",
+      },
     },
   },
-},
   plugins: [],
-}
+};
