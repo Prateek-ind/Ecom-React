dd3c32bfdbc46151b1e9ba00f7e3fecc0aecdcf1
Prateek <prateek.kumar.ind@gmail.com>
2025-10-10 15:36:09 +0530

    fix tailwind classes

--- src/components/ExploreSection/ExploreSection.jsx
+++ src/components/ExploreSection/ExploreSection.jsx
@@ -4,12 +4,19 @@ import Products from "../Products";
 
 const ExploreSection = () => {
   return (
-    <section className="w-full flex flex-col items-center px-4 lg:px-6 xl:px-8 mt-12 font-customFont uppercase">
+    <section
+      className="w-full flex flex-col items-center
+     px-4 lg:px-6 xl:px-8 mt-12 font-customFont bg-[#feffec] uppercase"
+    >
       <div className="flex flex-col items-center gap-4">
-        <p className="text-xs text-gray-900 font-light tracking-[2px]">Taste the variety of our premium snacks</p>
-        <h2 className="text-3xl font-normal text-gray-700 tracking-widest">Explore our flavours</h2>
+        <p className="text-xs text-gray-900 font-light tracking-[2px]">
+          Taste the variety of our premium snacks
+        </p>
+        <h2 className="text-3xl font-normal text-gray-700 tracking-widest">
+          Explore our flavours
+        </h2>
       </div>
-      <Products/>
+      <Products />
     </section>
   );
 };
