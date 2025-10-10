30cc3e353bad16fdd3bf4148d8a2b9d6dcfd9708
Prateek <prateek.kumar.ind@gmail.com>
2025-10-10 15:36:24 +0530

    use img from dummy api

--- src/components/HeroSection/HeroCarousel.jsx
+++ src/components/HeroSection/HeroCarousel.jsx
@@ -1,15 +1,13 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import HeroCarouselButtons from "./HeroCarouselButtons";
 import HeroShopButton from "./HeroShopButton";
+import { heroSectionImg } from "../../features/product/makhanaProducts";
 
 const HeroCarousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [loading, setLoading] = useState(true);
-  const imgSource = [
-    "//healthybuddie.com/cdn/shop/files/25_70117e57-b8e5-4b21-9e37-6d505a91a078.png?v=1757753866&width=5760",
-    "//healthybuddie.com/cdn/shop/files/16_1674dbb4-ecfa-4147-a6b7-68ea5fc90fbd.png?v=1751962604&width=3000",
-    "//healthybuddie.com/cdn/shop/files/22_120ee40c-dd86-4183-bc7a-edd5eb87c77c.png?v=1756285257&width=5760",
-  ];
+  const imgSource = Object.values(heroSectionImg);
+
   let imgIntervalId = "";
   useEffect(() => {
     imgIntervalId = setInterval(() => {
