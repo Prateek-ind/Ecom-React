a6a8c4938703046854fb732b8272bf9f1f144138
Prateek <prateek.kumar.ind@gmail.com>
2025-10-11 17:18:02 +0530

    uplift state

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,48 @@
 import HeroCarousel from "../components/HeroSection/HeroCarousel";
 import ExploreSection from "../components/ExploreSection/ExploreSection";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 const HomePage = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  const makhanaData = useSelector((state) => state.product.items?.[0]?.makhana);
+
+  const roastedMakhanaProductData = makhanaData?.roastedMakhana || {};
+  const roastedMakhanaComboProductData = makhanaData?.comboOfMakhana || {};
+
+  const roastedMakhanaProducts = Object.keys(roastedMakhanaProductData);
+  const roastedMakhanaComboProducts = Object.keys(
+    roastedMakhanaComboProductData
+  );
+  console.log(roastedMakhanaProductData);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+    window.addEventListener("resize", handleResize);
+  }, []);
+
   return (
     <main>
-      <HeroCarousel />
-      <ExploreSection/>
+      <HeroCarousel isMobile={isMobile} />
+      <ExploreSection
+        isMobile={isMobile}
+        heading="Explore our flavours"
+        slogan="Taste the variety of our premium snacks"
+        products={roastedMakhanaProducts}
+        productsData={roastedMakhanaProductData}
+        noOfItems={6}
+      />
+      <ExploreSection
+        isMobile={isMobile}
+        heading="Explore our flavours"
+        slogan="Taste the variety of our premium snacks"
+        products={roastedMakhanaComboProducts}
+        productsData={roastedMakhanaComboProductData}
+        noOfItems={3}
+      />
     </main>
   );
 };
