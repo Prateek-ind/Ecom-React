e20509b38bc5e2ea6035d2b6787ba3a249af3bdb
Prateek <prateek.kumar.ind@gmail.com>
2025-10-11 17:16:21 +0530

    View all products button

---
+++ src/components/ExploreSection/ViewAllBtn.jsx
@@ -0,0 +1,14 @@
+
+
+const ViewAllBtn = () => {
+  return (
+    <button
+      className="px-10 py-3 bg-lime-500 text-sm  border border-green-500
+    tracking-widest text-white mt-12 uppercase"
+    >
+      View All
+    </button>
+  );
+};
+
+export default ViewAllBtn;
