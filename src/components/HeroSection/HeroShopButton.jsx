b762840463ccec9a25aea15ba3fd74f986310acc
Prateek <prateek.kumar.ind@gmail.com>
2025-10-11 17:18:33 +0530

    css classes according to window size

--- src/components/HeroSection/HeroShopButton.jsx
+++ src/components/HeroSection/HeroShopButton.jsx
@@ -1,23 +1,46 @@
-const HeroShopButton = ({ currentIndex }) => {
+const HeroShopButton = ({ currentIndex, isMobile }) => {
+  const cssClasses = {
+    desktop: {
+      first: `" absolute z-10 top-70 right-50 px-16 py-2 font-lg
+         font-customFont text-[#80ad53] border border-[#80ad53] bg-white hover:scale-105
+         transition-all duration-500 hover:bg-gradient-to-l hover:from-white hover:to-transparent"`,
+
+      second: `" absolute z-10 right-12 bottom-12 px-16 py-2 font-lg
+         font-customFont text-[#80ad53] border border-[#80ad53] bg-white hover:scale-105
+         transition-all duration-500 hover:bg-gradient-to-l hover:from-white hover:to-transparent"`,
+    },
+    mobile: {
+      first: `" absolute z-10 top-70 left-5 px-16 py-2 font-lg
+         font-customFont text-[#80ad53] border border-[#80ad53] bg-white hover:scale-105
+         transition-all duration-500 hover:bg-gradient-to-l hover:from-white hover:to-transparent"`,
+
+      second: `" absolute z-10 right-50 bottom-50 px-16 py-2 font-lg
+         font-customFont text-[#80ad53] border border-[#80ad53] bg-white hover:scale-105
+         transition-all duration-500 hover:bg-gradient-to-l hover:from-white hover:to-transparent"`,
+    },
+  };
+
   if (currentIndex === 1) {
-    return <button
-      className=" absolute z-10 top-70 right-50 px-16 py-2 font-lg
-         font-customFont text-[#80ad53] border border-[#80ad53] bg-white hover:scale-105
-         transition-all duration-500 hover:bg-gradient-to-l hover:from-white hover:to-transparent"
-    >
-      Shop
-    </button>;
+    return (
+      <button
+        className={
+          isMobile ? cssClasses.mobile.first : cssClasses.desktop.first
+        }
+      >
+        Shop
+      </button>
+    );
+  } else {
+    return (
+      <button
+        className={
+          isMobile ? cssClasses.mobile.second : cssClasses.desktop.second
+        }
+      >
+        Shop
+      </button>
+    );
   }
-  else{
-    return  <button
-      className=" absolute z-10 right-12 bottom-12 px-16 py-2 font-lg
-         font-customFont text-[#80ad53] border border-[#80ad53] bg-white hover:scale-105
-         transition-all duration-500 hover:bg-gradient-to-l hover:from-white hover:to-transparent"
-    >
-      Shop
-    </button>
-  }
-  
 };
 
 export default HeroShopButton;
