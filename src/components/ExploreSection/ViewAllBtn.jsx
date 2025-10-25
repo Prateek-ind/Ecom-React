5a7f244112abf91d6b9aa0da6d024cf5ee3e3e08
Prateek <prateek.kumar.ind@gmail.com>
2025-10-25 19:19:07 +0530

    fix functionality

--- src/components/ExploreSection/ViewAllBtn.jsx
+++ src/components/ExploreSection/ViewAllBtn.jsx
@@ -1,12 +1,16 @@
-const ViewAllBtn = () => {
+import { Link } from "react-router";
+
+const ViewAllBtn = ({ category }) => {
   return (
-    <button
-      className="px-10 py-3 bg-[#63ce36] text-sm  border border-green-500
+    <Link to={`/view-All/${category}`}>
+      <button
+        className="px-10 py-3 bg-[#63ce36] text-sm  border border-green-500
     tracking-widest text-white mt-12 uppercase hover:scale-105 transition-all duration-500 hover:bg-gradient-to-l  hover:from-green-500 hover:to-green-800
          hover:text-white"
-    >
-      View All
-    </button>
+      >
+        View All
+      </button>
+    </Link>
   );
 };
 
