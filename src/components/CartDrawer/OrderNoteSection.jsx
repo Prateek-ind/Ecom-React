8dd740608c29b7f8b774508ef46a8fc5b1c73eb0
Prateek <prateek.kumar.ind@gmail.com>
2025-10-28 14:03:00 +0530

    font fixes

--- src/components/CartDrawer/OrderNoteSection.jsx
+++ src/components/CartDrawer/OrderNoteSection.jsx
@@ -18,7 +18,7 @@ const OrderNoteSection = ({ note, setNote, isEditing, setIsEditing }) => {
     <div className="mt-4">
       {!isEditing && !note && (
         <button
-          className="text-xs font-thin "
+          className="text-sm font-thin text-gray-700 "
           onClick={() => setIsEditing(true)}
         >
           Add order note.
@@ -37,13 +37,13 @@ const OrderNoteSection = ({ note, setNote, isEditing, setIsEditing }) => {
           />
           <div className="flex gap-4">
             <button
-              className="px-4 py-1 bg-green-500 text-white"
+              className="px-4 py-1 bg-[#63ce36] text-white"
               onClick={handleSave}
             >
               Save
             </button>
             <button
-              className="px-4 py-1 bg-white text-green-500"
+              className="px-4 py-1 bg-white text-[#63ce36]"
               onClick={handleCancel}
             >
               Cancel
@@ -53,13 +53,13 @@ const OrderNoteSection = ({ note, setNote, isEditing, setIsEditing }) => {
       )}
 
       {!isEditing && note && (
-        <div className="flex gap-2 items-center">
+        <div className="flex gap-2 items-center text-gray-700">
           <p>Order Note : </p>
           <p className=" border border-gray-200 outline-none text-sm font-thin px-4 py-2">
             {note}
           </p>
           <button
-            className="px-4 py-1 bg-green-500 text-white "
+            className="px-4 py-1 bg-[#63ce36] text-white "
             onClick={handleEdit}
           >
             Edit
