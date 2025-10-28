319f21c74effde62c01fba3457aadc4695fbe541
Prateek <prateek.kumar.ind@gmail.com>
2025-10-28 13:36:51 +0530

    create order note section

---
+++ src/components/CartDrawer/OrderNoteSection.jsx
@@ -0,0 +1,73 @@
+const OrderNoteSection = ({ note, setNote, isEditing, setIsEditing }) => {
+  const handleSave = () => {
+    if (note.trim() !== "") {
+      setIsEditing(false);
+    }
+  };
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleCancel = () => {
+    setNote("");
+    setIsEditing(false);
+  };
+
+  return (
+    <div className="mt-4">
+      {!isEditing && !note && (
+        <button
+          className="text-xs font-thin "
+          onClick={() => setIsEditing(true)}
+        >
+          Add order note.
+        </button>
+      )}
+
+      {isEditing && (
+        <div className=" flex flex-col gap-2 mt-4">
+          <textarea
+            className="w-full border border-gray-200 outline-none text-sm font-thin px-4 py-2"
+            name="order-note"
+            id="order-note"
+            placeholder="Add your note..."
+            value={note}
+            onChange={(e) => setNote(e.target.value)}
+          />
+          <div className="flex gap-4">
+            <button
+              className="px-4 py-1 bg-green-500 text-white"
+              onClick={handleSave}
+            >
+              Save
+            </button>
+            <button
+              className="px-4 py-1 bg-white text-green-500"
+              onClick={handleCancel}
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      )}
+
+      {!isEditing && note && (
+        <div className="flex gap-2 items-center">
+          <p>Order Note : </p>
+          <p className=" border border-gray-200 outline-none text-sm font-thin px-4 py-2">
+            {note}
+          </p>
+          <button
+            className="px-4 py-1 bg-green-500 text-white "
+            onClick={handleEdit}
+          >
+            Edit
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default OrderNoteSection;
