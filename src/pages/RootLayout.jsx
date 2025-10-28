a0122b4e6d7685a7a8326dc244e796492facea8b
Prateek <prateek.kumar.ind@gmail.com>
2025-10-28 13:38:02 +0530

    add cart drawer to root layout

--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import CartDrawer from "../components/CartDrawer";
+import CartDrawer from "../components/CartDrawer/CartDrawer";
 
 const RootLayout = () => {
   return (
