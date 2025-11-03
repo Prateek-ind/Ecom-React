e48dd28b91f329f09cf2548eb45f17bb786e83a5
Prateek <prateek.kumar.ind@gmail.com>
2025-11-03 20:52:15 +0530

    create checkout discount component

---
+++ src/components/CartDrawer/Checkout/CheckoutDiscount.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const CheckoutDiscount = () => {
+  return (
+    <div className="w-full flex gap-8 mt-4">
+      <input
+        type="text"
+        placeholder="Discount Code"
+        name="discount-coupon"
+        id="discount-coupon"
+        className="w-2/4 px-4 py-4 rounded"
+      />
+      <button className="bg-gray-200 px-4 py-2 rounded text-gray-500">Apply</button>
+    </div>
+  );
+};
+
+export default CheckoutDiscount;
