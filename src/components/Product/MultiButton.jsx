b2f61cd00aacf41e2f3be6794c52442a098acc54
Prateek <prateek.kumar.ind@gmail.com>
2025-11-18 12:40:14 +0530

    add moe functionality to multi btn

--- src/components/Product/MultiButton.jsx
+++ src/components/Product/MultiButton.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { useNavigate } from 'react-router';
 
-const MultiButton = ({label, className, navigateTo, closeCartDrawer=()=>{} }) => {
+const MultiButton = ({label, className, navigateTo, onClick=()=>{}, closeCartDrawer=()=>{} }) => {
     const navigate = useNavigate()
   return (
     <button
           className={className}
           onClick={() => {
-            navigate(navigateTo);
+            onClick()
+            navigate(navigateTo, {replace: false, relative: null});
             closeCartDrawer();
           }}
         >
