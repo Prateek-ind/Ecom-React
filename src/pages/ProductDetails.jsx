f67568b6bcc06b3de139e061230c539be59baac0
Prateek <prateek.kumar.ind@gmail.com>
2025-11-19 12:35:30 +0530

    refactor and use offer switcher component

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import MultiButton from "../components/Product/MultiButton";
 import { useDispatch } from "react-redux";
 import { cartActions } from "../features/cart/CartSlice";
+import OfferSwitcher from "../components/Product/OfferSwitcher";
 
 const offers = [
   { heading: "Free Shipping", 
@@ -17,24 +18,13 @@ const offers = [
 
 
 const ProductDetails = () => {
-  const [currentOfferIndex, setCurrentOfferIndex] = useState(0)
+  
   const dispatch = useDispatch()
   const location = useLocation()
   const product = location.state;
 
-  useEffect(() => {
-    const timerId = setInterval(() => {
-      setCurrentOfferIndex((prev)=>((prev+1)%offers.length))
-    }, 2000);
+ 
   
-    return () => {
-      clearInterval(timerId)
-    }
-  }, [])
-
-  const currentOffer = offers[currentOfferIndex]
-
-  console.log(currentOffer);
   return (
     <div className="pt-36 grid grid-cols-3 items-start">
       <div className="col-span-2">
@@ -53,10 +43,7 @@ const ProductDetails = () => {
           </div>
         </div>
         <EditQuantityComponent product={product} showRemoveBtn={false}/>
-        <div className="mt-8 bg-[#63ce36] text-white text-center rounded-lg py-8 px-4 tracking-wider">
-          <span className="text-xl p-2 font-bold bg-[#77ec45] rounded-full">{currentOffer.heading}</span>
-          <p className="mt-8 text-sm font-light leading-6">{currentOffer.content}</p>
-        </div>
+        <OfferSwitcher offers={offers}/>
         <MultiButton
           className="w-full px-8 py-2 bg-white text-[#63ce36] border border-[#63ce36] mt-4 mb-4 uppercase "
           navigateTo={'/cart'}
