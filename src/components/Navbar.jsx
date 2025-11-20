b30299ef2011de226a9ba0f31dd1c76840b3a17f
Prateek <prateek.kumar.ind@gmail.com>
2025-11-20 12:04:12 +0530

    define hover css on links in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
       <nav className=" w-full fixed z-50 mx-auto   bg-[#feffec]">
         <div className="mx-auto max-w-7xl px-4  lg:px-6  xl:px-8 flex items-center justify-between">
           <AiOutlineMenu size={28} className="block lg:hidden text-[#293819]" />
-          <Link to={'/'}>
+          <Link to={"/"}>
             <img
               src="//healthybuddie.com/cdn/shop/files/c62c0830-bad3-46e5-855d-273a3a5bc550.png?v=1744475062&width=1082"
               alt="logo"
@@ -23,12 +23,21 @@ const Navbar = () => {
             />
           </Link>
           <ul className="hidden lg:flex items-center gap-8 flex-wrap text-[#729b4a] text-xs font-normal font-customFont tracking-widest uppercase">
-            <Link className="px-2 py-12 border-b-2 border-transparent hover:border-[#729b4a]">Home</Link>
-            <Link className="px-2 py-12 border-b-2 border-transparent hover:border-[#729b4a]">Shop by category</Link>
-            <Link className="px-2 py-12 border-b-2 border-transparent hover:border-[#729b4a]">Combo</Link>
-            <Link className="px-2 py-12 border-b-2 border-transparent hover:border-[#729b4a]">Blogs and community</Link>
-            <Link className="px-2 py-12 border-b-2 border-transparent hover:border-[#729b4a]">Bulk order Enquiry</Link>
-            <Link className="px-2 py-12 border-b-2 border-transparent hover:border-[#729b4a]">Contact Us</Link>
+            <Link className="px-2 py-12 border-b-2 border-transparent hover:border-[#729b4a]">
+              Home
+            </Link>
+            <Link className="px-2 py-12 border-b-2 border-transparent hover:border-[#729b4a]">
+              Shop by category
+            </Link>
+            <Link className="px-2 py-12 border-b-2 border-transparent hover:border-[#729b4a]">
+              Combo
+            </Link>
+            <Link className="px-2 py-12 border-b-2 border-transparent hover:border-[#729b4a]">
+              Bulk order Enquiry
+            </Link>
+            <Link className="px-2 py-12 border-b-2 border-transparent hover:border-[#729b4a]">
+              Contact Us
+            </Link>
           </ul>
           <div className="flex items-center justify-between text-[#80ad53] gap-4">
             <AiOutlineUser size={28} />
