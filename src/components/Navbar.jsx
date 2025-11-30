ca16967dfb10b8d56ca62b455186416907f9b36a
Prateek <prateek.kumar.ind@gmail.com>
2025-11-30 16:08:45 +0530

    integrate both drawers in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,13 +6,18 @@ import {
 } from "react-icons/ai";
 import { useSelector } from "react-redux";
 import { Link } from "react-router";
+import ShopByCategoryDrawer from "./ShopByCategoryDrawer";
+import { useState } from "react";
+import ComboDrawer from "./ComboDrawer";
 
 const Navbar = () => {
+  const [hoverCategory, setHoverCategory] = useState(false);
+  const [hoverCombo, setHoverCombo] = useState(false);
   const cartItems = useSelector((state) => state.cart.items);
   const hasItems = Object.keys(cartItems).length;
   return (
     <>
-      <nav className=" w-full fixed z-50 mx-auto   bg-[#feffec]">
+      <nav className=" w-full fixed z-50 mx-auto bg-[#feffec]">
         <div className="mx-auto max-w-7xl px-4  lg:px-6  xl:px-8 flex items-center justify-between">
           <AiOutlineMenu size={28} className="block lg:hidden text-[#293819]" />
           <Link to={"/"}>
@@ -22,22 +27,51 @@ const Navbar = () => {
               className="w-24 cursor-pointer"
             />
           </Link>
-          <ul className="hidden lg:flex items-center gap-8 flex-wrap text-[#729b4a] text-xs font-normal font-customFont tracking-widest uppercase">
-            <Link className="px-2 py-12 border-b-2 border-transparent hover:border-[#729b4a]">
-              Home
-            </Link>
-            <Link className="px-2 py-12 border-b-2 border-transparent hover:border-[#729b4a]">
-              Shop by category
-            </Link>
-            <Link className="px-2 py-12 border-b-2 border-transparent hover:border-[#729b4a]">
-              Combo
-            </Link>
-            <Link className="px-2 py-12 border-b-2 border-transparent hover:border-[#729b4a]">
-              Bulk order Enquiry
-            </Link>
-            <Link className="px-2 py-12 border-b-2 border-transparent hover:border-[#729b4a]">
-              Contact Us
-            </Link>
+          <ul className="hidden lg:flex items-center gap-12 flex-wrap text-[#729b4a] text-xs font-normal font-customFont tracking-widest uppercase">
+            <li>
+              <Link className="px-2 py-12 border-b-2 border-transparent hover:border-[#729b4a]">
+                Home
+              </Link>
+            </li>
+            <li
+              onMouseEnter={() => setHoverCategory(true)}
+              onMouseLeave={() => setHoverCategory(false)}
+              className="relative"
+            >
+              <Link className="px-2 py-12 border-b-2 border-transparent hover:border-[#729b4a]">
+                Shop by category
+              </Link>
+              {hoverCategory && (
+                <div className="absolute left-0 w-full mt-10 px-4 py-4 border-t-2 border-[#729b4a] bg-[#feffec]">
+                  <ShopByCategoryDrawer />
+                </div>
+              )}
+            </li>
+            <li
+              onMouseEnter={() => setHoverCombo(true)}
+              onMouseLeave={() => setHoverCombo(false)}
+              className="relative"
+            >
+              <Link className=" px-2 py-12 border-b-2 border-transparent hover:border-[#729b4a]">
+                Combo
+              </Link>
+              {hoverCombo && (
+                <div className="absolute left-0 w-full mt-10 px-4 py-4 border-t-2 border-[#729b4a] bg-[#feffec]">
+                  <ComboDrawer />
+                </div>
+              )}
+            </li>
+
+            <li>
+              <Link className="px-2 py-12 border-b-2 border-transparent hover:border-[#729b4a]">
+                Bulk order Enquiry
+              </Link>
+            </li>
+            <li>
+              <Link className="px-2 py-12 border-b-2 border-transparent hover:border-[#729b4a]">
+                Contact Us
+              </Link>
+            </li>
           </ul>
           <div className="flex items-center justify-between text-[#80ad53] gap-4">
             <AiOutlineUser size={28} />
