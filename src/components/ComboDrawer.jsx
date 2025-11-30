7d30ec682b01397f6ff51d372e2142f22c7a65a9
Prateek <prateek.kumar.ind@gmail.com>
2025-11-30 16:08:21 +0530

    create combo drawer for navbar

---
+++ src/components/ComboDrawer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ComboDrawer = () => {
+  return (
+    <div>
+        <ul className="font-semibold">
+            <li className='pb-2 hover:text-[#729b4a7a] uppercase'>Makhana Combo</li>
+            <li className='pb-2 hover:text-[#729b4a7a] uppercase'>Tea Combo</li>
+        </ul>
+    </div>
+  )
+}
+
+export default ComboDrawer
