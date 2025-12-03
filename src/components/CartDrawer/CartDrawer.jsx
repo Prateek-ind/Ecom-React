aebe7aa828d815a16c44b8d2097138ddaac5f945
Prateek <prateek.kumar.ind@gmail.com>
2025-12-03 09:58:20 +0530

    fix width css

--- src/components/CartDrawer/CartDrawer.jsx
+++ src/components/CartDrawer/CartDrawer.jsx
@@ -18,10 +18,9 @@ const CartDrawer = () => {
     dispatch(cartUIActions.closeCart());
   };
 
-  console.log(cart);
   return (
     <div
-      className={`fixed top-0 right-0 h-full w-1/3 bg-white shadow-lg
+      className={`fixed top-0 right-0 h-full w-2/3 xl:w-1/3 bg-white shadow-lg
     transition-transform duration-300 z-50 flex flex-col
     ${isOpen ? "translate-x-0" : "translate-x-full"}
     `}
@@ -57,10 +56,9 @@ const CartDrawer = () => {
         </div>
         <MultiButton
           className="w-full px-8 py-2 bg-[#63ce36] text-white mt-4 mb-4 uppercase"
-          
-            navigateTo={"/cart/checkout"}
-            closeCartDrawer={closeCartDrawer}
-          label={'Checkout'}
+          navigateTo={"/cart/checkout"}
+          closeCartDrawer={closeCartDrawer}
+          label={"Checkout"}
         >
           Checkout
         </MultiButton>
