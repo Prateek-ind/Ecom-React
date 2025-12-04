d756d14dd5089f574dc6405d24d48bc1c31e30ad
Prateek <prateek.kumar.ind@gmail.com>
2025-12-04 10:15:48 +0530

    create bulk order inquiry fnc to connect to db

--- src/components/firebase/orderService.js
+++ src/components/firebase/orderService.js
@@ -1,7 +1,6 @@
-
 import { rdbUrl, setIdToken, getHeaders } from "../../features/cart/cartAPI";
 
-export const placeOrderToDb = async ({ orderDetails, userId,  }) => {
+export const placeOrderToDb = async ({ orderDetails, userId }) => {
   const url = `${rdbUrl}/orders/${userId}.json?auth=${setIdToken() || ""}`;
   const payload = {
     ...orderDetails,
@@ -20,20 +19,22 @@ export const placeOrderToDb = async ({ orderDetails, userId,  }) => {
   return await response.json();
 };
 
-export const bulkOrderInquiryToDb = async ({userId, orderDetails})=>{
-  const url = `${rdbUrl}/orders/bulkOrderInquiry/${userId}.json?auth=${setIdToken()|| ""}`
+export const bulkOrderInquiryToDb = async ({ userId, orderDetails }) => {
+  const url = `${rdbUrl}/bulkOrderInquiry/${userId}.json?auth=${
+    setIdToken() || ""
+  }`;
   const payload = {
     ...orderDetails,
     userId: userId || null,
     createdAt: new Date().toISOString(),
-  }
+  };
   const response = await fetch(url, {
     method: "POST",
     headers: getHeaders(),
-    body: JSON.stringify(payload)
-  })
+    body: JSON.stringify(payload),
+  });
   if (!response.ok) {
     throw new Error("Failed to place Order Inquiry");
   }
   return await response.json();
-}
+};
