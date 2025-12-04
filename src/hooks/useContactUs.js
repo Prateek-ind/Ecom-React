c88208c728980ac0a37647d8ae602bbc4abbba44
Prateek <prateek.kumar.ind@gmail.com>
2025-12-04 10:13:34 +0530

    create contact us hook to connect to DB

---
+++ src/hooks/useContactUs.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { contactUsToDb } from "../components/firebase/userService";
+
+export const useContactUs = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(null);
+
+
+  const contactUsFormSubmit = async ({ userId, messageDetails }) => {
+    setIsLoading(true);
+    setSuccess(false);
+    setError(null);
+
+    try {
+      const result = await contactUsToDb({ userId, messageDetails });
+      setSuccess(true);
+      return result;
+    } catch (error) {
+      setError("Failed to send Message." || error.message);
+      console.log(error.message);
+      return null;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return {contactUsFormSubmit, isLoading, success, error};
+};
