6f8eadbee6953b6465adc9e092c8254ae0e62bfc
Prateek <prateek.kumar.ind@gmail.com>
2026-01-10 15:01:59 +0530

    accept button type prop

--- src/components/ExploreSection/ViewAllBtn.jsx
+++ src/components/ExploreSection/ViewAllBtn.jsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router";
 
-const ViewAllBtn = ({ category }) => {
+const ViewAllBtn = ({ type, category }) => {
   return (
-    <Link to={`/view-All/${category}`}>
+    <Link to={`/view-All/${type}/${category}`}>
       <button
         className="px-10 py-3 bg-[#63ce36] text-sm  border border-green-500
     tracking-widest text-white mt-12 uppercase hover:scale-105 transition-all duration-500 hover:bg-gradient-to-l  hover:from-green-500 hover:to-green-800
