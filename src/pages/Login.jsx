960b963001af45a62883e77f0ff3f353e425941a
Prateek <prateek.kumar.ind@gmail.com>
2026-01-13 13:11:17 +0530

    remove checkout navbar

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,13 @@
-import React from "react";
 import LoginForm from "../components/LoginForm";
-import CheckoutNavbar from "../components/CartDrawer/Checkout/CheckoutNavbar";
 
 const Login = () => {
   return (
-    
     <>
-    <CheckoutNavbar/>
-    <div className="h-screen flex items-center justify-center">
-       
-      <div className="max-w-xl max-h-2xl bg-gray-200 rounded-xl">
-        
-        <LoginForm/>
+      <div className="h-screen flex items-center justify-center">
+        <div className="max-w-xl max-h-2xl bg-gray-200 rounded-xl">
+          <LoginForm />
+        </div>
       </div>
-    </div>
     </>
   );
 };
