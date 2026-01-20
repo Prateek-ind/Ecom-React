d5c3ab52d2670b12f2852436d7bce8df2d467452
Prateek <prateek.kumar.ind@gmail.com>
2026-01-20 11:51:29 +0530

    fix redux state path

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 import { Navigate } from "react-router";
 
 const ProtectedRoute = ({ children }) => {
-  const isLoggedIn = useSelector((state) => state.user.userId);
+  const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
   return isLoggedIn ? children : <Navigate to={"/auth/login"} replace />;
 };
 
