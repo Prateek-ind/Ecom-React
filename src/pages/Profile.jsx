0e73b1b507229a15488d4f7a7f52dcac905cf7a8
Prateek <prateek.kumar.ind@gmail.com>
2026-01-20 11:58:57 +0530

    fix firebase error and make redux as primary way to fetch profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,26 +1,32 @@
 import React, { useEffect } from "react";
 import ProfileForm from "../components/ProfileForm";
-import { auth } from "../components/firebase/config";
 
 import UserProfile from "../components/UserProfile";
 import { useUserProfile } from "../hooks/useUserProfile";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
-  const uid = auth.currentUser?.uid;
-  const { profile, isLoading, fetchUserProfile } = useUserProfile();
+  const { isLoading, fetchUserProfile } = useUserProfile();
+  const profile = useSelector((state) => state.userProfile);
+  console.log(profile)
+  const {isLoggedIn, uid} = useSelector((state) => state.user);
 
   useEffect(() => {
-    fetchUserProfile();
-    console.log(profile);
-  }, []);
+    if (isLoggedIn && uid && !profile.isLoaded) {
+       fetchUserProfile();
+      
+    }
+  }, [isLoggedIn, uid, fetchUserProfile]);
 
   if (isLoading) {
     return <p className="text-center">Loading profile...</p>;
   }
 
+  if (!isLoggedIn) return <p>Please login to view your profile</p>;
+
   return (
-    <section className="max-w-3xl mx-auto px-4 py-10 bg-[#feffec]">
-      <div className="mb-6 mt-24 text-center">
+    <section className="w-full mx-auto py-10 bg-[#feffec]">
+      <div className="mb-6 mt-28 text-center">
         <h2 className="text-4xl text-gray-700 tracking-widest mb-8 uppercase">
           Profile
         </h2>
@@ -30,7 +36,11 @@ const Profile = () => {
       </div>
       <hr />
 
-      {profile ? <UserProfile /> : <ProfileForm userId={uid} />}
+      {profile?.isLoaded && (profile.firstName || profile.lastName) ? (
+        <UserProfile profile={profile} />
+      ) : (
+        <ProfileForm />
+      )}
     </section>
   );
 };
