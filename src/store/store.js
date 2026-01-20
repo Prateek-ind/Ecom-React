cb8075b5bc67b176d3cdbcae17776e2098364218
Prateek <prateek.kumar.ind@gmail.com>
2026-01-20 11:59:23 +0530

    add userProfile reducer

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,7 @@ import cartReducer from "../features/cart/CartSlice";
 import cartUIReducer from "../features/cart/cartUISlice";
 import userReducer from "../features/users/userSlice"
 import { configureStore } from "@reduxjs/toolkit";
+import profileReducer from "../features/users/profileSlice"
 
 const store = configureStore({
   reducer: {
@@ -10,6 +11,7 @@ const store = configureStore({
     cartUI: cartUIReducer,
     product: productReducer,
     user: userReducer,
+    userProfile: profileReducer,
   },
 });
 
