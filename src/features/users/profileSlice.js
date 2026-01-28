e2a983aba07ca93230e363741f53aeb9d878c5e0
Prateek <prateek.kumar.ind@gmail.com>
2026-01-28 11:55:36 +0530

    refactored initial state and set profile reducer

--- src/features/users/profileSlice.js
+++ src/features/users/profileSlice.js
@@ -1,15 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  firstName: "",
-  lastName: "",
-  phoneNumber: "",
-  address: "",
-  addressOptional: "",
-  city: "",
-  state: "",
-  country: "India",
-  pinCode: "",
+  data: {
+    firstName: "",
+    lastName: "",
+    address: "",
+    addressOptional: "",
+    city: "",
+    state: "",
+    pinCode: "",
+    country: "India",
+    phoneNumber: "",
+  },
   isLoaded: false,
 };
 
@@ -18,16 +20,7 @@ const profileSlice = createSlice({
   initialState: initialState,
   reducers: {
     setProfile(state, action) {
-      const profileData = action.payload || {}
-      state.firstName = profileData.firstName || "";
-      state.lastName = profileData.lastName || "";
-      state.phoneNumber = profileData.phoneNumber || "";
-      state.address = profileData.address || "";
-      state.addressOptional = profileData.addressOptional || "";
-      state.city = profileData.city || "";
-      state.state = profileData.state || "";
-      state.country = profileData.country || "India";
-      state.pinCode = profileData.pinCode || "";
+      state.data = action.payload || {};
       state.isLoaded = true;
     },
     clearProfile() {
