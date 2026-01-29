52c58648453f2e06ed1291c06c009528a1458e23
Prateek <prateek.kumar.ind@gmail.com>
2026-01-29 09:29:14 +0530

    integrate link to single order and display details of orders

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Order = ({ orderDetails }) => {
   let orderPlacedDate = orderDetails.createdAt;
   orderPlacedDate = new Date(orderPlacedDate).toLocaleDateString();
   return (
-    <>
+    <Link
+      to={`/orderDetails?orderId=${orderDetails.orderId}`}
+    >
       <div
         className="px-4 py-4 grid grid-cols-7 grid-flow-col items-center
      gap-4 text-center border-b"
@@ -11,14 +15,20 @@ const Order = ({ orderDetails }) => {
         <p>{orderPlacedDate}</p>
         <p className="flex items-center">
           <img src={orderDetails.items[0].image} className="w-16 mr-4" alt="" />
-          {orderDetails.items.length > 1 &&<img src={orderDetails.items[1].image} className="w-16 mr-4" alt="" />}
+          {orderDetails.items.length > 1 && (
+            <img
+              src={orderDetails.items[1].image}
+              className="w-16 mr-4"
+              alt=""
+            />
+          )}
           {orderDetails.items.length > 2 && " + more..."}
         </p>
         <p>OrderStatus</p>
         <p>{orderDetails.payment.method}</p>
         <p>{orderDetails.pricing.total}</p>
       </div>
-    </>
+    </Link>
   );
 };
 
