f35bf07689762289dcc6f122592f5cc20fb6d318
Prateek <prateek.kumar.ind@gmail.com>
2026-02-04 13:24:56 +0530

    config error

--- src/services/firebase/config.js
+++ src/services/firebase/config.js
@@ -1,7 +1,7 @@
+/// <reference types="vite/client" />
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
-
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -16,6 +16,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app); 
-export const rdb = getDatabase(app); 
+export const auth = getAuth(app);
+export const rdb = getDatabase(app);
 export default app;
