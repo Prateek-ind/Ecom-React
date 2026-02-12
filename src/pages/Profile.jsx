8db078125e7e07d57a4b6afccb4d20ae76b4a8e5
Prateek Kumar <prateek.kumar.ind@gmail.com>
2026-02-12 20:47:29 +0530

    refactor for redux to tanstack shift

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,43 +1,69 @@
-import { useEffect } from "react";
+
 import ProfileForm from "@/features/users/components/ProfileForm";
 
 import UserProfile from "@/features/users/components/UserProfile";
-import { useUserProfile } from "../hooks/useUserProfile";
+// import { useUserProfile } from "../hooks/useUserProfile";
 import { useSelector } from "react-redux";
 import ProfileSkeleton from "../features/users/components/ProfileSkeleton";
+import { useQuery } from "@tanstack/react-query";
+import { fetchUserDetailsFromDb } from "../features/users/userAPI";
 
 const Profile = () => {
-  const { fetchUserProfile } = useUserProfile();
-  const profile = useSelector((state) => state.userProfile.data);
-  const isLoaded = useSelector((state) => state.userProfile.isLoaded);
-  const { isLoggedIn, uid } = useSelector((state) => state.auth);
-  useEffect(() => {
-    if (isLoggedIn && uid && !isLoaded) {
-      fetchUserProfile();
-    }
-  }, [isLoggedIn, uid, fetchUserProfile]);
+  const { uid, isLoggedIn } = useSelector((state) => state.auth);
+
+  const {
+    data: profile,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["profile", uid],
+    queryFn: () => fetchUserDetailsFromDb(uid),
+    enabled: !!uid && isLoggedIn,
+  });
 
   if (!isLoggedIn) return <p>Please login to view your profile</p>;
 
   const isProfileEmpty =
     !profile || (!profile.firstName && !profile.lastName && !profile.address);
 
-  if (!isLoaded) {
-    return <ProfileSkeleton/>;
+  if (isLoading) {
+    return <ProfileSkeleton />;
+  }
+
+  let content;
+
+  if (isError) {
+    content = (
+      <>
+        <div className="mb-6 mt-28 text-center">
+          <p>Error fetching profile details!!!</p>
+          <p>{error.message}</p>
+        </div>
+      </>
+    );
+  }
+
+  if (profile) {
+    content = (
+      <>
+        <div className="mb-6 mt-28 text-center">
+          <h2 className="text-4xl text-gray-700 tracking-widest mb-8 uppercase">
+            Profile
+          </h2>
+          {!isLoggedIn && (
+            <p className="text-xs text-gray-700 tracking-widest mb-8 italic">
+              Enter your details.
+            </p>
+          )}
+        </div>
+      </>
+    );
   }
 
   return (
     <section className="w-full mx-auto py-10 bg-[#feffec]">
-      <div className="mb-6 mt-28 text-center">
-        <h2 className="text-4xl text-gray-700 tracking-widest mb-8 uppercase">
-          Profile
-        </h2>
-        {!isLoggedIn && (
-          <p className="text-xs text-gray-700 tracking-widest mb-8 italic">
-            Enter your details.
-          </p>
-        )}
-      </div>
+      {content}
       <hr />
 
       {isProfileEmpty ? <ProfileForm /> : <UserProfile profile={profile} />}
