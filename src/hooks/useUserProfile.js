0b95350af248aa3f6700a2009692eae032022d38
Prateek Kumar <prateek.kumar.ind@gmail.com>
2026-02-12 20:46:06 +0530

    removed userProfile reducer as shifting to Tanstack

--- src/hooks/useUserProfile.js
+++ src/hooks/useUserProfile.js
@@ -1,58 +1,58 @@
-import { useCallback, useEffect, useState } from "react";
-import {
-  fetchUserDetailsFromDb,
-  storeUserDetailsToDb,
-} from "../features/users/userAPI";
-import { useDispatch, useSelector } from "react-redux";
-import { profileActions } from "../features/users/profileSlice";
+// import { useCallback, useEffect, useState } from "react";
+// import {
+//   fetchUserDetailsFromDb,
+//   storeUserDetailsToDb,
+// } from "../features/users/userAPI";
+// import { useDispatch, useSelector } from "react-redux";
+// import { profileActions } from "../features/users/profileSlice";
 
 
-export const useUserProfile = () => {
-  const dispatch = useDispatch();
-  const [isLoading, setIsLoading] = useState(false);
-  const uid = useSelector((state) => state.auth.uid);
+// export const useUserProfile = () => {
+//   const dispatch = useDispatch();
+//   const [isLoading, setIsLoading] = useState(false);
+//   const uid = useSelector((state) => state.auth.uid);
 
-  const fetchUserProfile = useCallback(async () => {
-    if (!uid) {
-      console.log('uid not found')
-      return null;
-    }
-    setIsLoading(true);
-    try {
-      const data = await fetchUserDetailsFromDb(uid);
-      dispatch(profileActions.setProfile(data));
-      return data;
-    } catch (error) {
-      console.error("Error fetching user profile:", error.message);
-      throw new Error(error.message);
-    } finally {
-      setIsLoading(false);
-    }
-  }, [uid, dispatch]);
+//   const fetchUserProfile = useCallback(async () => {
+//     if (!uid) {
+//       console.log('uid not found')
+//       return null;
+//     }
+//     setIsLoading(true);
+//     try {
+//       const data = await fetchUserDetailsFromDb(uid);
+//       dispatch(profileActions.setProfile(data));
+//       return data;
+//     } catch (error) {
+//       console.error("Error fetching user profile:", error.message);
+//       throw new Error(error.message);
+//     } finally {
+//       setIsLoading(false);
+//     }
+//   }, [uid, dispatch]);
 
-  const storeUserProfile = useCallback(
-    async (userDetails) => {
-      if (!uid) {
-         console.log("storeUserProfile called without uid");
-  return null;
-      }
-      setIsLoading(true);
-      try {
-        await storeUserDetailsToDb(userDetails, uid);
-        dispatch(profileActions.setProfile(userDetails));
-        return userDetails
-      } catch (error) {
-        console.error("Error storing user profile:", error.message);
-        throw new Error(error.message);
-      } finally {
-        setIsLoading(false);
-      }
-    },
-    [uid, dispatch]
-  );
+//   const storeUserProfile = useCallback(
+//     async (userDetails) => {
+//       if (!uid) {
+//          console.log("storeUserProfile called without uid");
+//   return null;
+//       }
+//       setIsLoading(true);
+//       try {
+//         await storeUserDetailsToDb(userDetails, uid);
+//         dispatch(profileActions.setProfile(userDetails));
+//         return userDetails
+//       } catch (error) {
+//         console.error("Error storing user profile:", error.message);
+//         throw new Error(error.message);
+//       } finally {
+//         setIsLoading(false);
+//       }
+//     },
+//     [uid, dispatch]
+//   );
 
-  useEffect(()=>{
-    fetchUserProfile()
-  }, [fetchUserProfile])
-  return { isLoading, fetchUserProfile, storeUserProfile };
-};
+//   useEffect(()=>{
+//     fetchUserProfile()
+//   }, [fetchUserProfile])
+//   return { isLoading, fetchUserProfile, storeUserProfile };
+// };
